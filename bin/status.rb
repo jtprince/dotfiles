ceec2c425bb28ffe0b2831056064dc7390c86967
John Prince <jtprince@gmail.com>
2013-05-30 13:56:47 -0600

    fixed status issue

--- bin/status.rb
+++ bin/status.rb
@@ -77,7 +77,7 @@ module Monitor
       # Kurt Vonnegut '"We are what we pretend to be, so we must be careful about what we pretend to be."
       'D&C 61:36 be of good cheer, little children; for I am in your midst, and I have not forsaken you.',
       'what you do is who you become',
-      'We are what we pretend to be, so careful what you pretend.",
+      'We are what we pretend to be, so careful what you pretend.',
     ]
 
     def get_data
