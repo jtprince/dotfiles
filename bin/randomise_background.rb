817f1fa3c277e8b55ecfe225a4c9851004f3cb62
John Prince <jtprince@gmail.com>
2013-09-01 08:49:57 -0600

    progress

--- bin/randomise_background.rb
+++ bin/randomise_background.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require 'shellwords'
 #File.write("/home/jtprince/randomize_bgk_which_ruby.log", "using ruby: #{RUBY_VERSION} #{RUBY_RELEASE_DATE} #{`which ruby`}")
 
 BKG_DIR = ENV['HOME'] + '/Dropbox/backgrounds'
@@ -8,12 +9,12 @@ EXTS = %w(png jpg)
 files_w_ext = Dir[*EXTS.map {|ext| BKG_DIR + "/*.#{ext}" }]
 
 bkg_prog = 
-  if `which feh`.size > 0
-    'feh --bg-fill'
-  elsif `which hsetroot`.size > 0
+  if `which hsetroot`.size > 0
     'hsetroot -fill'
+  elsif `which feh`.size > 0
+    'feh --bg-fill'
   else
     abort "need feh or hsetroot to work!"
   end
 
-system "#{bkg_prog} '#{files_w_ext.sample}'"
+system "#{bkg_prog} '#{Shellwords.escape(files_w_ext.sample)}'"
