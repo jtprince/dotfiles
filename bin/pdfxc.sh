b904db75a0f78358d7f996c51aad90d4affadec5
John Prince <jtprince@gmail.com>
2013-12-05 19:03:44 -0700

    progress

--- bin/pdfxc.sh
+++ bin/pdfxc.sh
@@ -1,4 +1,4 @@
 # expects an install into wine: http://www.docu-track.com/product/pdf-xchange-viewer
 
 echo "attempting to open $1"
-$HOME/Dropbox/env/pdfx/PDFXCview.exe $1 &
+wine $HOME/Dropbox/env/pdfx/PDFXCview.exe $1 &
