8156a63793dd6c0bcf526e944b57de571edc5ccc
John Prince <jtprince@gmail.com>
2014-01-07 19:11:42 -0700

    progress

--- bin/whatismyip.rb
+++ bin/whatismyip.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/ruby -w
 require 'open-uri'
 
-open('http://www.whatismyip.com/automation/n09230945.asp') do |x|
-  puts x.read
-end
+url = 'http://bot.whatismyipaddress.com'
+open(url) {|x| puts x.read }
