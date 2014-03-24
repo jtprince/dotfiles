00863d1c134d81de820bd8c63174f8af1797d93d
John Prince <jtprince@gmail.com>
2014-03-24 11:25:32 -0600

    progress

--- bin/email.rb
+++ bin/email.rb
@@ -4,24 +4,6 @@ require 'mail'
 require 'optparse'
 require 'io/console'
 
-## to is a string or an array
-#def send_email(from, to, subject, message)
-  #from_email = get_from_email(from) 
-  #to_email = get_to_email(to)
-
-  #msg = <<END_OF_MESSAGE
-#From: #{from}
-#To: #{to}
-#Subject: #{subject}
-
-##{message}
-#END_OF_MESSAGE
-
-  ## on local machine with smtp
-  #Net::SMTP.start('localhost') do |smtp|
-    #smtp.send_message(msg, from_email, *to_email)
-  #end
-#end
 emails = {
   gmail: '"John T. Prince" <jtprince@gmail.com>',
   chem: '"John T. Prince" <jtprince@chem.byu.edu>',
@@ -40,8 +22,10 @@ parser = OptionParser.new do |op|
     opt[:from] = v.include?('@') ? v : emails[v.to_sym]
   end
   op.on('-m', '--message <message>', "'my message' def: '#{opt[:message]}'") {|v| opt[:body] = v}
+  op.on('--msgfile <file>', "sets the message from <file> contents") {|v| opt[:body] = IO.read(v, encoding: 'UTF-8') }
   op.on('-s', '--subject <subject>', "'my subject' def: '#{opt[:subject]}'") {|v| opt[:subject] = v}
   op.on('-a', '--attachment <filepath>', "def: '#{opt[:attachments]}'") {|v| opt[:attachments] << v}
+  op.on('-d', '--dry', "don't actually send the email") {|v| opt[:dry] = v }
 end
 
 parser.parse!
@@ -51,18 +35,20 @@ if ARGV.size < 1
   exit
 end
 
-print "gmail password: "
-gmail_password = STDIN.noecho(&:gets).chomp
+unless opt[:dry]
+  print "gmail password: "
+  gmail_password = STDIN.noecho(&:gets).chomp
 
-Mail.defaults do
-  delivery_method :smtp, {
-    address: "smtp.gmail.com",
-    port: 587,
-    user_name: 'jtprince',
-    password: gmail_password,
-    authentication: :plain,
-    enable_starttls_auto: true,
-  }
+  Mail.defaults do
+    delivery_method :smtp, {
+      address: "smtp.gmail.com",
+      port: 587,
+      user_name: 'jtprince',
+      password: gmail_password,
+      authentication: :plain,
+      enable_starttls_auto: true,
+    }
+  end
 end
 
 opt[:to] = ARGV.to_a
@@ -78,7 +64,14 @@ attachments.each do |attachment|
   mail.add_file attachment
 end
 
-mail.deliver!
-
-
-
+if opt[:dry]
+  puts "=" * 78
+  puts mail.subject
+  puts "-" * 78
+  puts mail.body
+  puts "-" * 78
+  puts "ATTACHMENTS (num): (#{mail.attachments.size})"
+  puts "=" * 78
+else
+  mail.deliver!
+end
