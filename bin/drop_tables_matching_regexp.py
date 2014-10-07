9df08d3437818fa426e54a9aadf5c0a93a1576f8
John T. Prince <jtprince@gmail.com>
2014-10-07 12:56:53 -0600

    progress

---
+++ bin/drop_tables_matching_regexp.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import sys
+import sqlite3
+import argparse
+import re
+
+parser = argparse.ArgumentParser(description='deletes tables matching some regular expression')
+parser.add_argument('database', help='the database file')
+parser.add_argument('regexp', help='the regexp you are matching')
+parser.add_argument('--dry', action='store_true', help='just list the matching tables')
+args = parser.parse_args()
+
+conn = sqlite3.connect(args.database)
+c = conn.cursor()
+
+c.execute("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;")
+reply = c.fetchall()
+
+table_names = map(lambda tup: tup[0], reply)
+matching_tables = filter(lambda name: re.search(args.regexp, name), table_names)
+
+if args.dry:
+    print("\n".join(matching_tables))
+else:
+    for name in matching_tables:
+        c.execute("DROP TABLE %s" % name)
+
+conn.commit()
+conn.close()
