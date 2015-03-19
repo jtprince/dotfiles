bcce02d717025b16015eb893761a3b6027513e99
John T. Prince <jtprince@gmail.com>
2015-03-19 13:13:40 -0600

    progress

--- bin/django-test.py
+++ bin/django-test.py
@@ -8,45 +8,48 @@ import subprocess
 import sys
 from time import time
 
-parser = argparse.ArgumentParser(description="runs tests individually")
-parser.add_argument("files", default=[], nargs="*", help="test these files")
-parser.add_argument("-r", "--root", help="start looking for files")
-parser.add_argument("-s", "--start-finding-manage-dot-py", metavar='DIR', default=".", help="where to start looking for the manage.py file--only looks up")
-parser.add_argument("-m", "--regex", default=r'.', help="match specific files")
-parser.add_argument("--test-filter", default='test_*.py', help="a fnmatch filter")
-parser.add_argument("-t", "--test-regex", help="appends 'test_' to front of regex")
-parser.add_argument("-d", "--dry", action='store_true', help="don't run the tests")
+def regex(string):
+    return re.compile(string)
+
+parser = argparse.ArgumentParser(description="finds all test files and helps run them")
+parser.add_argument("-s", "--search", type=regex, help="search against specific test files")
+parser.add_argument("-e", "--exclude", type=regex, help="exclude specific test files")
+parser.add_argument("-i", "--individually", action='store_true', help="run tests individually")
+parser.add_argument("-d", "--dry", action='store_true', help="don't run, just print")
 args = parser.parse_args()
 
-def find_manage_dot_py(start_dir, find='manage.py'):
-    """ walk up the dir heirarchy until finding manage.py """
-    the_file = os.path.abspath(os.path.join(start_dir, find))
-    while not os.path.isfile(the_file):
-        find_manage_dot_py(os.path.join(start_dir, '..'))
-    return the_file
+root = '.'
 
-manage_dot_py = find_manage_dot_py(args.start_finding_manage_dot_py)
-if args.test_regex:
-    match = re.compile('test_' + args.test_regex)
-else:
-    match = re.compile(args.regex)
-
-matches = list(args.files)
-if not matches:
-    root = args.root or '.'
-    for root, sub_folders, filenames in os.walk(root):
-        for filename in filter(match.search, fnmatch.filter(filenames, args.test_filter)):
-            matches.append(os.path.join(root, filename))
-
-if args.dry or (len(sys.argv) == 1):
-    print("\n".join(matches))
-else:
-    for test_file in matches:
-        dot_file = test_file[:-3]
-        dot_file = re.sub(r'^./', '', dot_file)
-        dot_file = re.sub(r'/', '.', dot_file)
-        cmd = ["./manage.py", "test", dot_file]
-        print(" ".join(cmd))
-        start = time()
+def run_cmd(cmd):
+    start = time()
+    if not args.dry:
         subprocess.call(cmd)
         print("[Total Test Time: %f s]\n" % (time() - start))
+    else:
+        print(" ".join(cmd))
+
+def is_testfile(fname):
+    return re.match(r'test.*.py$', fname)
+
+def all_test_files(root):
+    matches = []
+    for root_, sub_folders, filenames in os.walk(root):
+        for filename in filter(is_testfile, filenames):
+            matches.append(os.path.join(root_, filename))
+    return matches
+
+files = all_test_files(root)
+
+if args.search:
+    files = filter(lambda fn: args.search.search(fn), files)
+
+if args.exclude:
+    files = filter(lambda fn: not args.exclude.search(fn), files)
+
+cmd = ["./manage.py", "test"]
+if args.individually:
+    for fn in files:
+        run_cmd(cmd + [fn])
+else:
+    start = time()
+    run_cmd(cmd + files)
