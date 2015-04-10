e5afe3c868e4d42e89506f18de0ece1264720f02
John T. Prince <jtprince@gmail.com>
2015-04-10 09:19:51 -0600

    progress

--- bin/whatismyip.rb
+++ bin/whatismyip.rb
@@ -1,5 +1,20 @@
 #!/usr/bin/ruby -w
-require 'open-uri'
 
-url = 'http://bot.whatismyipaddress.com'
-open(url) {|x| puts x.read }
+
+if ARGV[0] && ARGV[0][0] == 'l'
+  if `uname -r` =~ /ARCH/
+    reply = `ip addr`.split("\n")
+    addr_lines = reply.select do |line|
+      line =~ /inet /
+    end
+    addr_lines.each do |line|
+      puts line.split('/').first.strip.split(' ').last
+    end
+  else
+    puts "can't parse this distro ip addr yet"
+  end
+else
+  require 'open-uri'
+  url = 'http://bot.whatismyipaddress.com'
+  open(url) {|x| puts x.read }
+end
