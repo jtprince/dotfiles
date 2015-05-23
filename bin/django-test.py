59e8bc21fcba20cee54b114b6ac3fb3c7fd70f34
John T. Prince <jtprince@gmail.com>
2015-05-23 07:40:09 -0600

    progress

--- bin/django-test.py
+++ bin/django-test.py
@@ -12,6 +12,8 @@ def regex(string):
     return re.compile(string)
 
 parser = argparse.ArgumentParser(description="finds all test files and helps run them")
+parser.add_argument("files", nargs='*', help="files to test against.  Modified by --search or --exclude and overridden by --all")
+parser.add_argument("-a", "--all", action='store_true', help="start with all files")
 parser.add_argument("-s", "--search", type=regex, help="search against specific test files")
 parser.add_argument("-e", "--exclude", type=regex, help="exclude specific test files")
 parser.add_argument("-i", "--individually", action='store_true', help="run tests individually")
@@ -19,6 +21,8 @@ parser.add_argument("-d", "--dry", action='store_true', help="don't run, just pr
 parser.add_argument("--fresh-db", action='store_true', help="don't reuse the DB")
 parser.add_argument("--capture", action='store_true', help="don't use --nocapture")
 parser.add_argument("-n", "--test-on-network", action='store_true', help="activate tests using network connection")
+parser.add_argument("-v", "--verbosity", type=int, help="verbosity to pass in")
+parser.add_argument("--spec", action='store_true', help="use pinocchio's spec with color")
 
 args = parser.parse_args()
 
@@ -50,7 +54,10 @@ def all_test_files(root):
             matches.append(os.path.join(root_, filename))
     return matches
 
-files = all_test_files(root)
+if args.all:
+    files = all_test_files(root)
+else:
+    files = args.files
 
 if args.search:
     files = filter(lambda fn: args.search.search(fn), files)
@@ -69,6 +76,12 @@ cmd.append(test_type)
 
 if not args.capture:
     cmd.append("--nocapture")
+
+if args.verbosity:
+    cmd.extend(["--verbosity", str(args.verbosity)])
+
+if args.spec:
+    cmd.extend(["--with-spec", "--spec-color"])
 
 if args.individually:
     for fn in files:
