627e188dd37e821dddcce16ec451527d73bca2e8
John T. Prince <jtprince@gmail.com>
2015-11-28 20:42:27 -0700

    progress

--- bin/flac_to_mp3.rb
+++ bin/flac_to_mp3.rb
@@ -6,7 +6,8 @@ class String
   def shell_escape() Shellwords.escape(self) end
 end
 
-LAME_OPTS = "--preset extreme -q0"
+#LAME_OPTS = "--preset extreme -q0"
+LAME_OPTS = "--preset standard -q0"
 
 if ARGV.size == 0
   puts "usage: #{File.basename(__FILE__)} <file>.flac ..."
@@ -18,18 +19,22 @@ end
 
 
 tag_convert = {
-  :tt => "TITLE",
-  :tl => "ALBUM",
-  :ta => "ARTIST",
-  :tn => "TRACKNUMBER",
-  :tg => "GENRE",
-  :ty => "DATE",
+  tt: "TITLE",
+  tl: "ALBUM",
+  ta: "ARTIST",
+  tn: "TRACKNUMBER",
+  tg: "GENRE",
+  ty: "DATE",
 }
 
 ARGV.each do |file|
-  tag_opts = tag_convert.flat_map do |key,val|
-    data = `metaflac --show-tag=#{val} #{file.shell_escape}`.split("=",2).last.strip
-    ["--#{key}", data.shell_escape]
+  tag_opts = []
+  tag_convert.each do |key,val|
+    flactag = `metaflac --show-tag=#{val} #{file.shell_escape}`.strip
+    if flactag.size > 0
+      data = flactag.split("=",2).last.strip
+      tag_opts << "--#{key}" << data.shell_escape
+    end
   end
   mp3name = file.chomp(File.extname(file)) + ".mp3"
   cmd = "flac -dc #{file.shell_escape} | lame #{LAME_OPTS} #{tag_opts.join(" ")} --tc '#{LAME_OPTS}' --id3v2-only - #{mp3name.shell_escape}"
