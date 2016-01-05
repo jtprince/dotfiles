240d7edabd09e7a84cb5cfee1eb732c6d63f793b
John T. Prince <jtprince@gmail.com>
2016-01-05 14:50:53 -0700

    progress

---
+++ bin/stand-up.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output eDP1 --mode 1920x1080 --pos 1920x0 --rotate normal --output VGA1 --off
+overscan-correct
+randomise-bkg
