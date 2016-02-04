bc9cca09d30f8ff0d553459f2d385be4edaf304b
John T. Prince <jtprince@gmail.com>
2016-02-04 10:03:29 -0700

    progress

--- bin/doba-desk.sh
+++ bin/doba-desk.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP1 --off --output eDP1 --off --output VIRTUAL1 --off
+xrandr --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --mode 1920x1200 --pos 1920x0 --rotate normal --output DP1 --off --output eDP1 --off --output VIRTUAL1 --off
 randomise-bkg
