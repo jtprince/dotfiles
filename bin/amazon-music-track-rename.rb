dc1eba8274761521c410c1d31bdce1dc5ad6f940
John T. Prince <jtprince@gmail.com>
2016-03-07 10:19:14 -0700

    progress
    master

---
+++ bin/amazon-music-track-rename.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+files = ARGV
+
+if files.size == 0
+  puts "usage: #{File.basename(__FILE__)} <file>.mp3 ..."
+  puts "output: '3 - whatever' --> '003-whatever'"
+  puts "output: '15 - whatever' --> '015-whatever'"
+  exit
+end
+
+files.each do |file|
+  track, content = file.split(' - ', 2)
+  track = "0" + track if track.size == 2
+  track = "00" + track if track.size == 1
+  newname = track + '-' + content
+  File.rename(file, newname)
+end
