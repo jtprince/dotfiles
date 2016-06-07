0ab6a473665dd39e1198f4840651660c1c682153
John T. Prince <jtprince@gmail.com>
2016-06-06 18:57:45 -0600

    progress
    master

--- bin/sazed-laptop-solo.sh
+++ bin/sazed-laptop-solo.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-xrandr --output HDMI1 --off --output DP1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output VGA1 --off
+xrandr --output HDMI1 --off --output DP1 --off --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VGA1 --off
 randomise-bkg
