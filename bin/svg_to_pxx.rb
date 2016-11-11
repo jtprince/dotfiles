34649ccaa12e364d98064d29f93f7c30c0311649
John T. Prince <jtprince@gmail.com>
2016-11-10 17:45:46 -0700

    progress

--- bin/svg_to_pxx.rb
+++ bin/svg_to_pxx.rb
@@ -1,6 +1,13 @@
 #!/usr/bin/env ruby
 
+require 'shellwords'
 require 'optparse'
+
+class String
+  def esc
+    return Shellwords.escape(self)
+  end
+end
 
 opt = {
   :export_dpi => 300,
@@ -12,6 +19,8 @@ opts = OptionParser.new do |op|
   op.on("-d", "--export-dpi <#{opt[:export_dpi]}>", "rasterizes at this dpi") {|v| opt[:export_dpi] = v }
   op.on("-p", "--png", "export as png") {|v| opt[:png] = v }
   op.on("-b", "--bkg <color>", "export bkg color") {|v| opt[:bkg] = v }
+  op.on("--link", "assumes svglinkify.py magenta box links") {|v| opt[:linkify] = v }
+  op.on("--dry", "just print out the command") {|v| opt[:dry] = v }
 end
 
 opts.parse!
@@ -21,23 +30,59 @@ if ARGV.size == 0
   exit
 end
 
+def run(cmd, opt)
+  puts cmd
+  unless opt[:dry]
+    system cmd
+  end
+end
+
+def build_conversion_cmd(file, outfile, opt)
+  outflag =
+    if outfile =~ /\.pdf$/
+      "-A"
+    elsif outfile =~ /\.png$/
+      "-e"
+    end
+  args = [
+    "inkscape",
+    "-f", file.esc,
+    opt[:text_to_path] ? "-t" : nil,
+    outfile =~ /\.svg$/ ? nil : "-d #{opt[:export_dpi]}",
+    opt[:bkg] ? "-b #{opt[:bkg]}" : nil,
+    outflag, outfile.esc,
+  ]
+  return args.compact.join(" ")
+end
+
 ARGV.each do |file|
   base = file.chomp(File.extname(file))
-  (ext, out_flag) = 
+  ext =
     if opt[:png]
-      ['.png', '-e']
+      '.png'
     else
-      ['.pdf', '-A']
+      '.pdf'
     end
+
   outfile = base + ext
-  cmds = []
-  cmds << "inkscape"
-  cmds << "-f" << "'#{file}'"
-  cmds << "-t" if opt[:text_to_path]
-  cmds << "-d" << opt[:export_dpi] 
-  (cmds << "-b" << opt[:bkg])  if opt[:bkg]
-  cmds << out_flag << outfile
-  cmd = cmds.join(" ")
-  puts cmd
-  system cmd
+
+  outfile_for_conversion = opt[:linkify] ? outfile.sub(".pdf", ".linkify.pdf") : outfile
+
+  cmd = build_conversion_cmd(file, outfile_for_conversion, opt)
+  run cmd, opt
+
+  if opt[:linkify]
+    svg_outfile = base + ".linkify.svg"
+    if file =~ /\.svgz/
+      run "gunzip < #{file.esc} > #{svg_outfile}", opt
+    else
+      svg_outfile = file
+    end
+    linkify_cmd = ["svglinkify.py", svg_outfile, outfile_for_conversion, outfile].join(" ")
+    run linkify_cmd, opt
+    if svg_outfile =~ /\.linkify.svg$/
+      File.unlink(svg_outfile) if File.exist?(svg_outfile)
+    end
+    File.unlink(outfile_for_conversion) if File.exist?(outfile_for_conversion)
+  end
 end
