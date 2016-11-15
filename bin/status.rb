49ca1a8f3b4f575873ac0058855f2d73883a0393
John T. Prince <jtprince@gmail.com>
2016-11-14 22:30:14 -0700

    progress

--- bin/status.rb
+++ bin/status.rb
@@ -221,10 +221,10 @@ module SysMonitor
 
     def get_data
       data = YAML.load(`spotify-info`)
-      (artist, album, title, tracknum) = ['artist', 'album', 'title'].map do |key|
+      (artist, album, title) = ['artist', 'album', 'title'].map do |key|
         shorten(data['xesam:' + key])
       end
-      "#{artist} (#{title}) #{data['xesam:trackNumber']}-#{title}"
+      "#{artist} (#{album}) #{data['xesam:trackNumber']}-#{title}"
     end
   end
 end
