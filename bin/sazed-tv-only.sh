bf797bf9e6abe1bf006750e2f3e99a3002958123
John T. Prince <jtprince@gmail.com>
2016-12-24 15:20:39 -0700

    changes

--- bin/sazed-tv-only.sh
+++ bin/sazed-tv-only.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 xrandr --output HDMI1 --mode 1920x1080 --primary --pos 0x0 --rotate normal --output DP1 --off --output eDP1 --off --output VGA1 --off
-overscan-correct
+# overscan picture looks nice with tv, but set to computer and then you can
+# use HDMI output with TV
+#overscan-correct
 randomise-bkg
