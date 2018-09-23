048bc7ef0a4581836af0f631065d1338a3e77e02
John T. Prince <jtprince@gmail.com>
2018-09-22 23:02:34 -0600

    more

--- bin/reddit-inbox-tray.py
+++ bin/reddit-inbox-tray.py
@@ -4,6 +4,7 @@ import sys
 import time
 from threading import Thread
 
+import yaml
 import praw
 import gi
 # these must come after import gi and before other imports
@@ -17,41 +18,46 @@ from gi.repository import Gtk  # noqa: E402
 username = sys.argv[1]
 
 user_agent = ("User-Agent: linux.reddit-inbox-tray:v0.0.1 (by /u/bwv549)")
-reddit = praw.Reddit(user_agent=user_agent)
 
-with open(f"/home/jtprince/Dropbox/env/reddit/{username}.password") as infile:
-    password = infile.read()
+REDDIT_ENV_PATH = "/home/jtprince/Dropbox/env/reddit"
+# ICON_PATH = "
+# ICON_PATH = "/home/jtprince/Dropbox/env/reddit/icons"
+client_data = "dev_client.yml"
+MESSAGES_ICON = 'gtk3-demo'
+NO_MESSAGES_ICON = 'gtk-3-demo-symbolic.symbolic'
 
-reddit.login(username = 'bwv549', password = password)
+with open(f"{REDDIT_ENV_PATH}/{client_data}") as infile:
+    client_data = yaml.load(infile)
 
-mail = "false"
+with open(f"{REDDIT_ENV_PATH}/{username}.password") as infile:
+    password = infile.read().rstrip()
 
-for msg in reddit.get_unread(limit=1):
-    print(msg)
-    mail = "true"
+login_credentials = dict(
+    user_agent=user_agent, username=username, password=password, **client_data
+)
 
-exit(1)
+reddit = praw.Reddit(**login_credentials)
 
 
 class MessageIndicator:
-    UPDATE_SECONDS = 30
+    UPDATE_SECONDS = 5
 
     def __init__(self):
-        self.app = 'test123'
-        iconpath = '/usr/share/icons/Adwaita/16x16/status/'
+        self.app = 'reddit-messages-tray'
 
-        self.indicator = AppIndicator3.Indicator.new_with_path(
+        self.indicator = AppIndicator3.Indicator.new(
             self.app,
             icon_name='reddit messages',
-            icon_theme_path=iconpath,
             category=AppIndicator3.IndicatorCategory.OTHER
         )
         self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
-        self.indicator.set_menu(self.create_menu())
-        iconpath = '/usr/share/icons/Adwaita/16x16/status'
-        icon_name = "/usr/share/icons/Adwaita/16x16/status/appointment-soon.png"
-        icon_name = "appointment-soon"
-        self.indicator.set_icon_full(icon_name, "no messages")
+
+        have_unread_mail = self.have_unread_mail()
+        icon = MESSAGES_ICON if have_unread_mail else NO_MESSAGES_ICON
+        messages = "have messages" if have_unread_mail else "no messages"
+
+        self.indicator.set_icon_full(icon, messages)
+
         # self.indicator.set_label("1 Monkey", self.app)
         # the thread:
         self.update = Thread(target=self.check_messages)
@@ -76,14 +82,29 @@ class MessageIndicator:
         menu.show_all()
         return menu
 
+    def have_unread_mail(self):
+        _have_unread_mail = False
+        for msg in reddit.inbox.unread(limit=1):
+            _have_unread_mail = True
+
+        print("have unread", _have_unread_mail)
+
+        return _have_unread_mail
+
     def check_messages(self):
         while True:
             time.sleep(self.UPDATE_SECONDS)
             # apply the interface update using  GLib.idle_add()
+
+            have_unread_mail = self.have_unread_mail()
+            icon = MESSAGES_ICON if have_unread_mail else NO_MESSAGES_ICON
+            messages = "have messages" if have_unread_mail else "no messages"
+            print(icon)
+
             GLib.idle_add(
                 self.indicator.set_icon_full,
-                "changes-prevent",
-                "message",
+                icon,
+                messages,
                 priority=GLib.PRIORITY_DEFAULT
             )
 
