a09499b69040e352f9a8ac89fa8c02d30bcbaf93
John T. Prince <jtprince@gmail.com>
2019-04-22 13:12:20 -0600

    customize for 3plcentral

---
+++ bin/luce-1920-1200.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+xrandr \
+    --output HDMI1 --primary --mode 1920x1200 --pos 0x0 --rotate normal \
+    --output HDMI2 --off \
+    --output DP1 --off \
+    --output eDP1 --off \
+    --output VIRTUAL1 --off
+
+randomise-bkg
+xmodmap ~/.config/xmodmap
