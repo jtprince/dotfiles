e7f585a611bb9a7a79020ffbd8db6b7ac6a27587
John T. Prince <jtprince@gmail.com>
2019-05-15 11:42:47 -0600

    device naming dynamic for kelsier

---
+++ bin/kelsier-asus1920x1080.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+
+
+device_lines = `xrandr`.split("\n").select {|line| line =~ /^\w.*connected/ }
+
+# with kelsier, the devices show up as eDP-1 or sometimes as eDP1
+devices = device_lines.map {|line| line.split(/\s/).first }
+
+dp_devices = devices.select {|device| device =~ /^DP\d/ }
+dp3 = dp_devices.pop
+
+
+def off(device)
+  "--output #{device} --off"
+end
+
+def on(device, resolution)
+    "--output #{device} --primary --mode #{resolution} --pos 0x0 --rotate normal"
+end
+
+cmd = [
+  'xrandr',
+  on(dp3, "1920x1080"),
+  off(devices.first),
+  *dp_devices.map {|dp_device| off(dp_device) }
+].join(' ')
+
+`#{cmd}`
+
+`randomise-bkg`
+`xmodmap ~/.config/xmodmap`
---
+++ bin/kelsier-laptop-solo.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+edp1_resolution = '3840x2160'
+
+def off(device)
+  "--output #{device} --off"
+end
+
+def on(device, resolution)
+    "--output #{device} --primary --mode #{resolution} --pos 0x0 --rotate normal"
+end
+
+device_lines = `xrandr`.split("\n").select {|line| line =~ /^\w.*connected/ }
+
+# with kelsier, the devices show up as eDP-1 or sometimes as eDP1
+devices = device_lines.map {|line| line.split(/\s/).first }
+edp1_device = devices.first
+
+dp_devices = devices.select {|device| device =~ /^DP\d/ }
+
+cmd = [
+  'xrandr',
+  on(edp1_device, edp1_resolution),
+  *dp_devices.map {|dp_device| off(dp_device) }
+].join(' ')
+
+`#{cmd}`
+
+`randomise-bkg`
+`xmodmap ~/.config/xmodmap`
