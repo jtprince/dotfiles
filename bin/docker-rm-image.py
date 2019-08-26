94e711a96b46ffe9c740089f0c6ce89412a8aa99
John T. Prince <jtprince@gmail.com>
2019-08-26 15:37:54 -0600

    docker rm image

---
+++ bin/docker-rm-image.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+# from argcomplete.completers import ChoicesCompleter
+from subprocess import run
+from argparse import ArgumentParser
+
+# TODO: interpret image id as an id (can do a simple match to existing ids)
+# and accept that also
+# TODO: show image ids if there are images with the same name.
+
+def _get_image_names():
+    image_lines = run(["docker", "images", "-a"], capture_output=True, encoding="utf-8").stdout.split("\n")
+    image_lines.pop(0)
+    return [line.split()[0] for line in image_lines if line]
+
+parser = ArgumentParser()
+parser.add_argument("name", nargs="*", help="the name of the image")
+parser.add_argument("--list", "-l", action="store_true", help="list image names and exit")
+
+
+# .completer = ChoicesCompleter(image_names)
+args = parser.parse_args()
+if args.list or not args.name:
+    print("IMAGE NAMES:")
+    for name in _get_image_names():
+        print("  ", name)
+
+
+for name in args.name:
+    completed = run(["docker", "images", "-q", name], capture_output=True, encoding="utf-8")
+    image_id = completed.stdout.split("\n")[0].strip()
+    run(["docker", "image", "rm", image_id])
