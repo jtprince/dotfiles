f4f0a332e8a77adccf75d2e8e90a10ba30a5058f
John T. Prince <jtprince@gmail.com>
2020-03-17 16:21:19 -0600

    mute the sink-input, not the sink for spotify ad blocker

--- bin/spotify-block-ads.py
+++ bin/spotify-block-ads.py
@@ -2,8 +2,10 @@
 
 # pip install --user yaml python-daemon
 
+import re
 import subprocess
 import time
+from itertools import groupby
 
 import yaml
 
@@ -35,6 +37,15 @@ import yaml
 
 ADVERTISEMENT = ('Advertisement', 'Spotify')
 
+SINK_INPUT_RE = re.compile(r'Sink Input #(\d+)')
+
+_SPOTIFY_REGEXS = [
+    r'media\.role = "music"',
+    r'media\.name = "Spotify"',
+    r'application\.name = "Spotify"',
+    r'application\.process.binary = "spotify"'
+]
+SPOTIFY_REGEXS = [re.compile(regex, re.MULTILINE) for regex in _SPOTIFY_REGEXS]
 
 def in_advertisement():
     info = subprocess.run("spotify-info", capture_output=True)
@@ -42,40 +53,25 @@ def in_advertisement():
     return song_data['xesam:title'] in ADVERTISEMENT
 
 
-def get_spotify_sink():
-    client_data = subprocess.run("pactl list short clients".split(), capture_output=True, encoding='utf-8').stdout
-    channels = []
-    for client_line in client_data.split("\n"):
-        client_data = client_line.split("\t")
-        client = client_data[-1]
-        if client == 'spotify':
-            channels.append(int(client_data[0]))
-
-    channel_to_sink = {}
-    sink_str = subprocess.run("pactl list short sink-inputs".split(), capture_output=True, encoding='utf-8').stdout
-    for sink_line in sink_str.split("\n"):
-        sink_data = sink_line.split("\t")
-        if sink_data[0]:
-            print(sink_data)
-            channel = int(sink_data[2])
-            sink = int(sink_data[1])
-            channel_to_sink[channel] = sink
-
-    spotify_sink = None
-    for channel in channels:
-        if channel in channel_to_sink:
-            spotify_sink = channel_to_sink[channel]
-            break
-
-    return spotify_sink
+def get_spotify_sink_input():
+    """ Returns sink input # of the first spotify instance found. """
+    client_data = subprocess.run("pactl list sink-inputs".split(), capture_output=True, text=True).stdout
+    lines = client_data.split("\n")
+    sink_inputs = [
+        "\n".join(group) for not_matched, group in groupby(lines, lambda line: not line)
+        if not not_matched
+    ]
+    for sink_input in sink_inputs:
+        if all([regex.search(sink_input) for regex in SPOTIFY_REGEXS]):
+            return SINK_INPUT_RE.match(sink_input).group(1)
 
 
 def mute_sink(sink):
-    subprocess.run(f"pactl set-sink-mute {sink} on".split())
+    subprocess.run(f"pactl set-sink-input-mute {sink} on".split())
 
 
 def unmute_sink(sink):
-    subprocess.run(f"pactl set-sink-mute {sink} off".split())
+    subprocess.run(f"pactl set-sink-input-mute {sink} off".split())
 
 
 def run():
@@ -86,12 +82,12 @@ def run():
             if not in_advertisement_state:
                 # print("BEGINNING MUTE")
                 in_advertisement_state = True
-                spotify_sink = get_spotify_sink()
-                mute_sink(spotify_sink)
+                sink_input = get_spotify_sink_input()
+                mute_sink(sink_input)
         elif in_advertisement_state:
             # print("UNMUTING")
-            spotify_sink = get_spotify_sink()
-            unmute_sink(spotify_sink)
+            sink_input = get_spotify_sink_input()
+            unmute_sink(sink_input)
             in_advertisement_state = False
 
         time.sleep(1)
@@ -104,3 +100,67 @@ def run():
 
 if __name__ == '__main__':
     run()
+
+
+# Sink Input #81
+#         Driver: protocol-native.c
+#         Owner Module: 12
+#         Client: 330
+#         Sink: 1
+#         Sample Specification: s16le 2ch 44100Hz
+#         Channel Map: front-left,front-right
+#         Format: pcm, format.sample_format = "\"s16le\""  format.rate = "44100"  format.channels = "2"  format.channel_map = "\"front-left,front-right\""
+#         Corked: no
+#         Mute: no
+#         Volume: front-left: 65535 / 100% / -0.00 dB,   front-right: 65535 / 100% / -0.00 dB
+#                 balance 0.00
+#         Buffer Latency: 1021587 usec
+#         Sink Latency: 26655 usec
+#         Resample method: n/a
+#         Properties:
+#                 media.role = "music"
+#                 media.name = "Spotify"
+#                 application.name = "Spotify"
+#                 native-protocol.peer = "UNIX socket client"
+#                 native-protocol.version = "33"
+#                 application.process.id = "388443"
+#                 application.process.user = "jtprince"
+#                 application.process.host = "kelsier"
+#                 application.process.binary = "spotify"
+#                 window.x11.display = ":0"
+#                 application.language = "en_US.UTF-8"
+#                 application.process.machine_id = "6b79ee180b6b41caadbfafef34d81be2"
+#                 application.process.session_id = "1"
+#                 application.icon_name = "spotify-client"
+#                 module-stream-restore.id = "sink-input-by-media-role:music"
+
+# Sink Input #87
+#         Driver: protocol-native.c
+#         Owner Module: 12
+#         Client: 341
+#         Sink: 1
+#         Sample Specification: float32le 2ch 44100Hz
+#         Channel Map: front-left,front-right
+#         Format: pcm, format.sample_format = "\"float32le\""  format.rate = "44100"  format.channels = "2"  format.channel_map = "\"front-left,front-right\""
+#         Corked: no
+#         Mute: no
+#         Volume: front-left: 65172 /  99% / -0.15 dB,   front-right: 65172 /  99% / -0.15 dB
+#                 balance 0.00
+#         Buffer Latency: 35532 usec
+#         Sink Latency: 26447 usec
+#         Resample method: copy
+#         Properties:
+#                 application.icon_name = "chromium-browser"
+#                 media.name = "Playback"
+#                 application.name = "Chromium"
+#                 native-protocol.peer = "UNIX socket client"
+#                 native-protocol.version = "33"
+#                 application.process.id = "16085"
+#                 application.process.user = "jtprince"
+#                 application.process.host = "kelsier"
+#                 application.process.binary = "chromium"
+#                 application.language = "en_US.UTF-8"
+#                 window.x11.display = ":0"
+#                 application.process.machine_id = "6b79ee180b6b41caadbfafef34d81be2"
+#                 application.process.session_id = "1"
+		# module-stream-restore.id = "sink-input-by-application-name:Chromium"
