b61f38cd81b02af3ac22497e605fdb26b8b7cb57
John T. Prince <jtprince@gmail.com>
2020-11-05 18:17:45 -0700

    either way

--- bin/vin-hdmi.sh
+++ bin/vin-hdmi.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-xrandr --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
-    --output eDP-1 --off \
-    --output DP-1 --off
+xrandr --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1 --off --output DP-1 --off
+xrandr --output HDMI-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1-1 --off --output DP-1-1 --off
 randomise-bkg
 xmodmap ~/.config/xmodmap
