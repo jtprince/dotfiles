17e16da828ee9894ae7445a5f28635f2c9777b2d
John T. Prince <jtprince@gmail.com>
2021-01-15 14:05:30 -0700

    misc

--- bin/helm-secret-extract-google-key.py
+++ bin/helm-secret-extract-google-key.py
@@ -13,7 +13,14 @@ parser = argparse.ArgumentParser(
     description="Extracts google creds from a helm secret.*.yaml file"
 )
 parser.add_argument("secret_file", type=Path, help="a secrets.*.yaml file")
-parser.add_argument("-o", "--outfile", type=Path, help="outfile or stdout")
+parser.add_argument(
+    "-o",
+    "--outfile",
+    type=Path,
+    help=str(
+        "defaults to stdout " "(safe to capture stdout with redirects instead)"
+    ),
+)
 parser.add_argument(
     "--no-cleanup", action="store_true", help="don't remove decrypted file"
 )
@@ -24,19 +31,22 @@ args = parser.parse_args()
 base = ["helm-decrypt", "--force"]
 cmd = base + [str(args.secret_file)]
 
-subprocess.run(cmd)
+subprocess.check_call(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 decrypted = args.secret_file.with_suffix(args.secret_file.suffix + ".dec")
+if not decrypted.exists():
+    raise RuntimeError(f"Failed to create decrypted file {str(decrypted)}")
 
 data = yaml.safe_load(decrypted.open())
 
 if not args.no_cleanup:
-    print(f"clean up removing file: {str(decrypted)}")
+    print(f"clean up removing file: {str(decrypted)}", file=sys.stderr)
     decrypted.unlink()
 
 base64_encoded = data["secrets"]["googleKey"]
 google_json = base64.b64decode(base64_encoded).decode("utf-8")
 
 out = args.outfile.open("w") if args.outfile else sys.stdout
+
 
 out.write(google_json)
