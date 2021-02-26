7c5c9a3ad4682c9f45a826fd3d6ec920bbc0af37
John T. Prince <jtprince@gmail.com>
2021-02-26 11:29:49 -0700

    init work on i3-sway config

--- config/i3-sway/build.py
+++ config/i3-sway/build.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env python
 
-import subprocess
+from functools import partial
 from pathlib import Path
+from subprocess import run
 
 this_dir = Path(__file__).resolve().parent
 
 template_file = this_dir / "config.mo"
+preamble = this_dir / "preamble.txt"
 
 current_dir = Path.cwd()
 
 yaml_file = list(current_dir.glob("*.yaml"))[0]
 
-cmd = f"mustache {str(yaml_file)} {str(template_file)} > config"
-subprocess.run(cmd, shell=True)
+cmd = f"mustache {str(yaml_file)} {str(template_file)} > _config_no_preamble"
+
+runshell = partial(run, shell=True)
+
+runshell(cmd)
+
+cmd = f"cat {str(preamble)} _config_no_preamble > config"
+runshell(cmd)
+
+runshell("rm _config_no_preamble")
