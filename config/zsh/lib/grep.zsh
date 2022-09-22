84165e79cf86d0e74a65c79b59800a32e3c36c05
John T. Prince <jtprince@gmail.com>
2022-09-22 14:18:52 -0600

    pq concat

--- config/zsh/lib/grep.zsh
+++ config/zsh/lib/grep.zsh
@@ -3,7 +3,8 @@
 # Examples: http://rubyurl.com/ZXv
 #
 # export GREP_OPTIONS='--color=auto'
-export GREP_COLOR='1;32'
+# export GREP_COLOR='1;32'
+export GREP_COLORS='mt=1;32'
 
 function grep {
     command grep --color=auto "$@"
