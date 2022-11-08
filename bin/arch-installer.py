089c7343dc75b60f99cd8744c457808443fef776
John T. Prince <jtprince@gmail.com>
2022-11-07 22:36:37 -0700

    arch installer changes

--- bin/arch-installer.py
+++ bin/arch-installer.py
@@ -1,76 +1,8 @@
-#!/usr/bin/env python
+pyenv: isort: command not found
 
-import argparse
-import subprocess
-from pathlib import Path
-import os
+The `isort' command exists in these Python versions:
+  3.9.13/envs/xtomap-3.9.13
+  xtomap-3.9.13
 
-from ruyaml import YAML
-
-YAML_PATH = Path.home() / "dotfiles/config/arch/installation.yaml"
-
-# Sometimes a python package needs to be installed
-os.environ['PIP_REQUIRE_VIRTUALENV'] = 'false'
-
-
-def partition(condition, iterable):
-    trues = []
-    falses = []
-    for item in iterable:
-        if condition(item):
-            trues.append(item)
-        else:
-            falses.append(item)
-    return trues, falses
-
-
-def install_subsection(data):
-    special, packages_to_install = partition(
-        lambda item: isinstance(item, dict), data
-    )
-    cmd = ["yay", "-S", "--noconfirm"]
-    subprocess.run(cmd + packages_to_install)
-    for item in special:
-        if post_commands := item.get("_post_commands"):
-            print()
-            print("-" * 70)
-            print("Now run the following commands:")
-            print("-" * 70)
-            for command in post_commands:
-                print(command)
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--video",
-    choices=["intel", "amd", "nvidia"],
-    help="which video card",
-)
-parser.add_argument(
-    "--hostname",
-    help="name of the host",
-)
-parser.add_argument(
-    "--yaml-path", default=YAML_PATH, help="path to the yaml installation file"
-)
-parser.add_argument(
-    "--install-subsections", help="section.subsection[,subsection]"
-)
-
-args = parser.parse_args()
-
-yaml = YAML(typ="safe")
-doc = yaml.load(args.yaml_path)
-
-if args.install_subsections:
-    if "." in args.install_subsections:
-        section, subsections_str = args.install_subsections.split(".", 1)
-        subsections = subsections_str.split(",")
-        section_data = doc[section]
-        section_keys = list(section_data.keys())
-        section_keys_display = "\n".join(section_keys)
-        for subsection in subsections:
-            if subsection not in section_data:
-                print(f"AVAILABLE KEYS: {section_keys_display}")
-            subsection_data = section_data[subsection]
-            install_subsection(subsection_data)
+Note: See 'pyenv help global' for tips on allowing both
+      python2 and python3 to be found.
