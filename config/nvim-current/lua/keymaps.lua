209e18bb9bd93f46d7d079e693a1a80a7419b8bd
John T. Prince <jtprince@gmail.com>
2024-03-19 14:35:57 -0600

    misc

--- config/nvim-current/lua/keymaps.lua
+++ config/nvim-current/lua/keymaps.lua
@@ -53,6 +53,8 @@ vimp.noremap("D", "<PAGEUP>M")
 -- TODO: make all the keyboard stuff consistent
 vim.keymap.set('n', ',y', '"*y', { noremap = true, silent = true, desc = 'yank to primary clipboard' })
 vim.keymap.set('n', ',Y', '"+y', { noremap = true, silent = true, desc = 'yank to secondary clipboard' })
+vim.keymap.set('n', '<leader>y', '"*y', { noremap = true, silent = true, desc = 'yank to primary clipboard' })
+vim.keymap.set('n', '<leader>Y', '"+y', { noremap = true, silent = true, desc = 'yank to secondary clipboard' })
 vim.api.nvim_set_keymap('n', 'hh', 'gg', { noremap = true, silent = true, desc = 'jump to top of file' })
 -- vim.api.nvim_set_keymap('n', 'gg', 'G', { noremap = true, silent = true, desc = 'jump to bottom of file' })
 
