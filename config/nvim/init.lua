3990eea0658caebdfaaed3c136596c401e7e3cd2
John T. Prince <jtprince@gmail.com>
2026-04-24 11:39:41 -0600

    add rainbow csv

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -181,6 +181,29 @@ require("lazy").setup({
 	}),
 
 	-------------------------------------------------------------------------
+	-- Rainbow CSV
+	-------------------------------------------------------------------------
+	unless_vscode({
+		"cameron-wags/rainbow_csv.nvim",
+		config = true,
+		ft = {
+			"csv",
+			"tsv",
+			"csv_semicolon",
+			"csv_whitespace",
+			"csv_pipe",
+			"rfc_csv",
+			"rfc_semicolon",
+		},
+		cmd = {
+			"RainbowDelim",
+			"RainbowDelimSimple",
+			"RainbowDelimQuoted",
+			"RainbowMultiDelim",
+		},
+	}),
+
+	-------------------------------------------------------------------------
 	-- ToggleTerm
 	-------------------------------------------------------------------------
 	unless_vscode({
