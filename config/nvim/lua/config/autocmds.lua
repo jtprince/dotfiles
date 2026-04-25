8242b99fa362f4fb3ffc4a6239bd2364e180d412
John T. Prince <jtprince@gmail.com>
2026-04-25 03:34:02 -0600

    better session mgmt for nvim, fix1

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -47,6 +47,14 @@ vim.api.nvim_create_autocmd("VimEnter", {
 		vim.cmd("cd " .. vim.fn.fnameescape(root))
 		require("persistence").load()
 		vim.notify("Session: " .. vim.fn.fnamemodify(root, ":~"), vim.log.levels.INFO)
+		-- Re-open files passed on cmdline (session restore wipes them).
+		if vim.fn.argc() > 0 then
+			vim.schedule(function()
+				for i = 0, vim.fn.argc() - 1 do
+					vim.cmd("edit " .. vim.fn.fnameescape(vim.fn.argv(i)))
+				end
+			end)
+		end
 	end,
 })
 
